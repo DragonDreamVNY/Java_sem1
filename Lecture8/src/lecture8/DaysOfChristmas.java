bfad13be21696ea1ac25f5c647ede3b34c68f270
dragonboyvny - bitbucket <vinnylee@gmail.com>
2016-10-10 00:25:49 +0100

    trying DoWhile Loops exercises
    Failing miserably.
    Understand Armstrong Numbers.
    Not getting the print numbers in a row of 8 elements!

--- Lecture8/src/lecture8/DaysOfChristmas.java
+++ Lecture8/src/lecture8/DaysOfChristmas.java
@@ -1,3 +1,5 @@
+package lecture8;
+
 class DaysOfChristmas {
 
     public static void main(String[] args) {
--- k00223361_Armstrong [unfinished]/src/armstrong/Armstrong.java
+++ k00223361_Armstrong [unfinished]/src/armstrong/Armstrong.java
@@ -10,31 +10,33 @@ public class Armstrong {
 
     public static void main(String[] args) {
         //variables
-        final int MAX = 999;
-        int n =100;
-        int a, b, c, compare, armNum;
-        String output = "List of Armstrong numbers between " + n + " and " + MAX + "\n";
+        final int MAX = 999; //for assurance that MAX will always be 999
+        int n =100; // starts at 100 but will change
+        int a, b, c, compare, armNum; // first, second, third Digits, a temporary comparison Number, and end result ArmStrong Number
+        String output = "List of Armstrong numbers between " + n + " and " + MAX + "\n"; // going to append  += the output with mix of String and Results.
         
 
-            while(n < MAX){
+            while (n < MAX){
                 compare = n; //assign value to temporary. Break this value up to three digits to compare
                 
                 /*--- extra first, second, third digits ---*/
                 
                 //extract the first number
-                a = compare/100;
+                a = compare/100; //ie. 213 / 100 = [2] [ - ] [ - ]
                 
                  // extract the second number
-                compare = compare%100;   
+                compare = compare % 100;    // ie. 213 % 100 = [ ] [1] [3] , assigned to itself,
                 
-                b= compare / 10; 
+                b= compare / 10;  //ie. 13 / 10 = [1] [ - ]
                 
                 // extract the third number
-                compare = compare % 10;
+                compare = compare % 10; // ie. 13 % 10 = [ - ] [3]
                         // is the remaining digit.
-                c =compare; 
-
+                c =compare; //ie. c is the same as temparary.
+                
+                // this is the formula for ArmStrong Number  ( abc  = a^3 +b^3 + c^3 )
                  armNum = (int) Math.pow(a, 3) + (int) Math.pow(b, 3) + (int) Math.pow(c, 3);
+                 
                 //  if total == n you have an Armstrong number
                 if ( armNum == n ){  
                      
