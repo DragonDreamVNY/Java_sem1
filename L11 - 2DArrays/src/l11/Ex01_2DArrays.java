d83174065a1aea20ff2665e0b80c7f37fb69e391
dragonboyvny - bitbucket <vinnylee@gmail.com>
2016-10-19 01:33:58 +0100

    cleared Array  Ex5 random with Modal
    Fixed 2D Arrays ex 1.
    To Do: Ex 6 & 7
    To Do: 2D Arrays exercises+

--- L11 - 2DArrays/src/l11/Ex01_2DArrays.java
+++ L11 - 2DArrays/src/l11/Ex01_2DArrays.java
@@ -4,69 +4,52 @@ import java.util.Random;
 import javax.swing.JOptionPane;
 
 /**
- *
  * @author DragonDream
  *  Ex 1 2D Arrays
  */
 public class Ex01_2DArrays {
 
- 
-    
     public static void main(String[] args) {
         
         int matrix[][] = new int[3][4];
         Random rand = new Random();
-        
         // generate Random numbers and populate array  in range 0..110
         for (int r = 0 ; r < matrix.length; r++){ //rows 0, 1, 2
-            
             for (int c = 0; c < 4; c++){ //columns 0, 1, 2, 3
                 matrix [r][c] = rand.nextInt(111); //populate the matrix array with random values in range {0..110}
                 System.out.print( matrix [r][c] + " "); //print each element in the Row...
             } // end column For
+           
             System.out.println(""); //new line for each Row
         } //end row For
         
-        int largest = matrix[0][0];
+        int largest = matrix[0][0]; //large and small Variables for comparing/holding
         int smallest = matrix[0][0];
-        
-        String outSmall = "";
+
         String outLarge = "";
+        String outSmall = "";
         
         for (int r = 0; r < matrix.length; r++){ //row
+            largest = 0;
+            smallest = 110;
             for (int c = 0; c < 4; c++) { //col
-                
+                //reset both values each loop before comparing again to the value at the new Index [c] at row [r]
+               
                 // check largest
                 if (matrix[r][c] > largest) {
                     largest = matrix[r][c];
-                    //outLarge += "\nThe largest number in row " + r + " is ";
-                    //outLarge += largest;
-                    System.out.println("LARGEST Number at row " + r + "\t" + largest);
                 } // end if
                 
                 //  check smallest
                 if (matrix[r][c] < smallest){
                     smallest = matrix[r][c];
-                    outSmall += ("The smallest number in row " + r + " is " + smallest);
                 }
-                
-               /*
-                outSmall += "The smallest number in col " + r + " : " ;
-                System.out.println("\nSmallest is " + outSmall); 
-                */
-               
-            } // end Matrix column For
-            
-            outLarge += "\n"; //new line for each Row
-            outSmall += "\n"; //new line for each Row
-            
+            } // end Matrix column For    
+            System.out.println("The largest number on Row " + r  + " is " + largest);
+            System.out.println("The smallest number on Row " + r  + " is " + smallest);
         } //end Matrix row For
-         System.out.println(outLarge);
-         System.out.println(outSmall);
-        //  JOptionPane.showMessageDialog(null, outLarge);
-        // JOptionPane.showMessageDialog(null, outSmall);
-       
         
+       
     } //end main
     
 } // end class
--- Lecture10_Arrays/src/l10/arrays/Arrays05.java
+++ Lecture10_Arrays/src/l10/arrays/Arrays05.java
@@ -1,6 +1,7 @@
 
 package l10.arrays;
 import javax.swing.JOptionPane;
+import java.util.Random;
 
 /**
  *
@@ -14,9 +15,47 @@ public class Arrays05 {
 
     public static void main(String[] args) {
        
+        int randomNums[] = new int[5000]; //array with 5000 elements
+        int freq[] = new int[30]; //array for 30 values {0..29}
+        int min = 0; //variables for limits to Random
+        int max = 29;
         
+        Random randomVal = new Random(); // special 'Random' variable generating Random Number value
+        int randomTemp; //variable for use in the loop before assigning to Array elements
         
+        int modal = 0; 
+        int compValue = 0;
         
+        // ------------------------------------------
+        // array with 5000 random values in range 0..29
+        //------------------------------------------
+        for( int i = 0; i<randomNums.length; i++){
+            randomTemp = randomVal.nextInt((max - min) +1) +min; //randomTemp at this index is assigned a random number
+            randomNums[i] = randomTemp; //the same randomTemp is assigned to the Array Element at this index..
+            
+            freq[randomTemp]++; //add a counter for this particular random value in the freq array, 
+        } // end for RANDOMS
+        
+        // ------------------------------------------
+        // Print out the number of occurences each 
+        //------------------------------------------
+        System.out.println("Frequency Table");
+        for (int i = 0; i < freq.length; i++) {
+            System.out.println("\'" + i + "\' occured " + freq[i] + " times"); //freq[i] array has values stored from last part..
+        }
+        
+        // ------------------------------------------
+        // compare frequency value at each index to 
+        //------------------------------------------
+        compValue = freq[0]; // reset at index 0 to check values 
+        for (int i = 0; i <freq.length; i++){
+            
+            if (freq[i] > compValue){
+                modal = i;
+                compValue = freq[i];
+            } //end if       
+        }// end for FREQUENCY
+        System.out.println("\nModal Value is " + modal + " which occured " + compValue  + " times.");
         
     } // end main
 } // end class
